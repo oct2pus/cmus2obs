9755424d5e34bc10b8d204b315fcb88ab763a246
Octopus Octopus <oct2pus@jade.moe>
2024-03-13 22:17:52 -0500

    basic loop, mp3 support, default image, resize all images to same size

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module cmus2obs
 go 1.22.1
 
 require (
-	github.com/go-flac/flacpicture v0.3.0 // indirect
-	github.com/go-flac/go-flac v1.0.0 // indirect
+	github.com/bogem/id3v2/v2 v2.1.4
+	github.com/go-flac/flacpicture v0.3.0
+	github.com/go-flac/go-flac v1.0.0
+	golang.org/x/image v0.15.0
 )
+
+require golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,60 +1,92 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"image"
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"os/exec"
 
+	"github.com/bogem/id3v2/v2"
 	"github.com/go-flac/flacpicture"
 	"github.com/go-flac/go-flac"
+	"golang.org/x/image/draw"
+
+	_ "image/gif"
+	"image/jpeg"
+	_ "image/jpeg"
+	_ "image/png"
 )
 
-const TIMER = 2
+const (
+	TIMER      = 2
+	IMAGE_SIZE = 700
+)
 
 func main() {
-	c := exec.Command("cmus-remote", "-Q")
-	o, _ := c.Output()
-	remoteResp := strings.Split(string(o), "\n")
-	//Album
-	album, err := getAttribute(remoteResp, "tag album ")
-	if err != nil {
-		album = "Unknown"
-	}
-	//artist
-	artist, err := getAttribute(remoteResp, "tag artist ")
-	if err != nil {
-		artist = "Unknown"
-	}
-	// Title
-	title, err := getAttribute(remoteResp, "tag title ")
-	if err != nil {
-		title = "Unknown"
-	}
-
-	filepath, err := getAttribute(remoteResp, "file ")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	img := make([]byte, 0)
-	if strings.HasSuffix(filepath, ".flac") {
-		img, err = getFlacArt(filepath)
+	for {
+		c := exec.Command("cmus-remote", "-Q")
+		o, _ := c.Output()
+		remoteResp := strings.Split(string(o), "\n")
+		//Album
+		album, err := getAttribute(remoteResp, "tag album ")
 		if err != nil {
+			album = "Unknown"
+		}
+		//artist
+		artist, err := getAttribute(remoteResp, "tag artist ")
+		if err != nil {
+			artist = "Unknown"
+		}
+		// Title
+		title, err := getAttribute(remoteResp, "tag title ")
+		if err != nil {
+			title = "Unknown"
+		}
+
+		filepath, err := getAttribute(remoteResp, "file ")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		img := make([]byte, 0)
+		if strings.HasSuffix(filepath, ".flac") {
+			img, err = getFlacArt(filepath)
+			if err != nil {
+				img = defaultArt()
+			}
+		} else if strings.HasSuffix(filepath, ".mp3") {
+			img, err = getMP3Art(filepath)
+			if err != nil {
+				img = defaultArt()
+			}
+		} else {
 			img = defaultArt()
 		}
-	} else if strings.HasSuffix(filepath, ".mp3") {
-		getMP3Art()
-	} else {
-		defaultArt()
-	}
+		imgBuff := bytes.NewBuffer(img)
 
-	writeTxt("SongAlbum", album)
-	writeTxt("SongArtist", artist)
-	writeTxt("SongTitle", title)
-	writeJpg("AlbumArt", img)
+		imgOrig, _, err := image.Decode(imgBuff)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		imgOut := image.NewRGBA(image.Rect(0, 0, IMAGE_SIZE, IMAGE_SIZE))
+
+		draw.BiLinear.Scale(imgOut, imgOut.Rect, imgOrig, imgOrig.Bounds(), draw.Over, nil)
+
+		jpeg.Encode(imgBuff, imgOut, nil)
+
+		writeTxt("SongAlbum", album)
+		writeTxt("SongArtist", artist)
+		writeTxt("SongTitle", title)
+		writeJpg("AlbumArt", img)
+
+		time.Sleep(TIMER * time.Second)
+	}
 }
 
 func getFlacArt(s string) ([]byte, error) {
@@ -71,15 +103,37 @@ func getFlacArt(s string) ([]byte, error) {
 	return nil, errors.New("no image found")
 }
 
-func getMP3Art() {
-
+func getMP3Art(s string) ([]byte, error) {
+	tags, err := id3v2.Open(s, id3v2.Options{Parse: true})
+	if err != nil {
+		return nil, errors.New("can't open mp3 tags")
+	}
+	defer tags.Close()
+	pic := tags.GetFrames(tags.CommonID("Attached picture"))
+	if pic != nil {
+		return pic[0].(id3v2.PictureFrame).Picture, nil //i literally do not care if it has multiple pictures, pick the first one.
+	}
+	return nil, errors.New("no image found")
 }
 
 func defaultArt() []byte {
-	return nil
+	file, err := os.Open("default.jpg")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer file.Close()
+	info, err := file.Stat()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	art := make([]byte, info.Size())
+
+	file.Read(art)
+	return art
 }
 
 func writeJpg(filename string, input []byte) {
+	os.Mkdir("output", 0755)
 	f, err := os.Create("./output/" + filename + ".jpg")
 	if err != nil {
 		log.Fatal(err.Error())
@@ -90,6 +144,7 @@ func writeJpg(filename string, input []byte) {
 }
 
 func writeTxt(filename, input string) {
+	os.Mkdir("output", 0755)
 	f, err := os.Create("./output/" + filename + ".txt")
 	if err != nil {
 		log.Fatal(err.Error())
